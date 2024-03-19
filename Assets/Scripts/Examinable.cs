feaf46c598105df0e2305d6fd427846a2a0312e3
simon <simonpham1294@gmail.com>
2024-03-18 22:54:40 -0300

    Added object examining app

--- Assets/Scripts/Examinable.cs
+++ Assets/Scripts/Examinable.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class Examinable : MonoBehaviour
 {
-   
+    private ExaminableManager _examinableManager;
+
+    private void Start()
+    {
+        _examinableManager = GameObject.FindObjectOfType<ExaminableManager>();
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+    }
+
+    public void RequestExamine()
+    {
+        _examinableManager.PerformExamine(this);
+    }
+
+    public void RequestUnexamine()
+    {
+        _examinableManager.PerformUnexamine();
+    }
 }
--- Assets/Scripts/ExaminableManager.cs
+++ Assets/Scripts/ExaminableManager.cs
@@ -2,7 +2,30 @@ using UnityEngine;
 
 public class ExaminableManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _examineTarget;
+    [SerializeField] private Transform _examineTarget;
 
+    private Vector3 _cachedPos;
+    private Quaternion _cachedRot;
+    private Examinable _currentExaminedObj;
 
+    public void PerformExamine(Examinable examinable)
+    {
+        _currentExaminedObj = examinable;
+
+        if(_currentExaminedObj != null)
+        {
+            _cachedPos = _currentExaminedObj.transform.position;
+            _cachedRot = _currentExaminedObj.transform.rotation;
+
+            _currentExaminedObj.transform.position = _examineTarget.position;
+            _currentExaminedObj.transform.parent = _examineTarget;
+        } 
+    }
+
+    public void PerformUnexamine()
+    {
+        _currentExaminedObj.transform.position = _cachedPos;
+        _currentExaminedObj.transform.rotation = _cachedRot;
+        _currentExaminedObj.transform.parent = null;
+    }
 }
