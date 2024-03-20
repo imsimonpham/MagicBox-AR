a9c4629a1f8ded7635ea38aae9382809ac938764
simon <simonpham1294@gmail.com>
2024-03-20 20:35:00 -0300

    Updated examinable and examinable manager script

--- Assets/Scripts/ExaminableManager.cs
+++ Assets/Scripts/ExaminableManager.cs
@@ -7,7 +7,7 @@ public class ExaminableManager : MonoBehaviour
     private Vector3 _cachedPos;
     private Quaternion _cachedRot;
     private Examinable _currentExaminedObj;
-
+    private Transform _cachedParent;
     private Vector3 _cachedScale;
 
     private bool _isExamining = false;
@@ -31,35 +31,30 @@ public class ExaminableManager : MonoBehaviour
 
     public void PerformExamine(Examinable examinable)
     {
+        _isExamining = true;
         _currentExaminedObj = examinable;
 
-        if(_currentExaminedObj != null)
-        {
-            //cache the examinable transform data so we can reset it
-            _cachedPos = _currentExaminedObj.transform.position;
-            _cachedRot = _currentExaminedObj.transform.rotation;
-            _cachedScale = _currentExaminedObj.transform.localScale;
+        //cache the examinable transform data so we can revert it later
+        _cachedPos = _currentExaminedObj.transform.localPosition;
+        _cachedRot = _currentExaminedObj.transform.localRotation; 
+        _cachedParent = _currentExaminedObj.transform.parent;
+        _cachedScale = _currentExaminedObj.transform.localScale;
 
-            //move the examinable to the target position and assign a parent to it
-            _currentExaminedObj.transform.position = _examineTarget.position;
-            _currentExaminedObj.transform.parent = _examineTarget;
+        //move the examinable to the target position and assign a parent to it
+        _currentExaminedObj.transform.position = _examineTarget.position;
+        _currentExaminedObj.transform.parent = _examineTarget;
 
-            //Offset the scale to fit the examinable in view
-            Vector3 offsetScale = _cachedScale *  examinable.examineScaleOffset;
-            _currentExaminedObj.transform.localScale = offsetScale;
-
-            _isExamining = true;
-        } 
+        //Offset the scale to fit the examinable view
+        _currentExaminedObj.transform.localScale = _cachedScale * examinable.examineScaleOffset;
     }
 
     public void PerformUnexamine()
     {
-        _currentExaminedObj.transform.position = _cachedPos;
-        _currentExaminedObj.transform.rotation = _cachedRot;
-        _currentExaminedObj.transform.localScale = _cachedScale;
-
-        _currentExaminedObj.transform.parent = null;
-        _currentExaminedObj = null;
         _isExamining = false;
+
+        _currentExaminedObj.transform.parent = _cachedParent;
+        _currentExaminedObj.transform.localPosition = _cachedPos;
+        _currentExaminedObj.transform.localRotation = _cachedRot;
+        _currentExaminedObj.transform.localScale = _cachedScale;
     }
 }
